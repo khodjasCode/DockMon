023cc991b4d4f97c13e1d8bfec6476062a89fb4c
khodjasCode <asrorkhodjatursunkhodjayev@gmail.com>
2025-11-12 15:03:08 +0500

    modified prometheus file

--- app/main.py
+++ app/main.py
@@ -19,9 +19,6 @@ app.config["SECRET_KEY"] = SECRET_KEY
 def index():
     return render_template('index.html' , containers = get_containers())
 
-# @app.route('/containers')
-# def containers():
-#     return jsonify(get_containers())
 
 @app.route('/stats/<string:id>')
 def stats(id):
@@ -30,24 +27,6 @@ def stats(id):
     print(container_stats)
     return render_template('stats.html', stats = container_stats , container_id=id)
 
-# @app.route('/stats/<string:id>')
-# def api_stats(id):
-#     container_stats = get_container_by_id(id)
-#     if not container_stats:
-#         return jsonify({'error': 'not found'}), 404
-#     return jsonify(container_stats)
-
-# @app.route('/stats/<string:id>/data')
-# def stats_data(id):
-#     container_stats = get_container_by_id(id)
-    
-#     # Верните нужные данные в JSON формате
-#     return jsonify({
-#         'id': id,
-#         'memory': container_stats.get('memory', 'N/A'),
-#         'cpu': container_stats.get('cpu', 'N/A'),
-#         # добавьте другие поля которые нужны
-#     })
 
 @app.route('/container/<string:id>/start', methods=['POST'])
 def start_container(id):
